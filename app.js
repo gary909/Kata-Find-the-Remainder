a3b6566b97b05b27708fe6af3dc43834b03bd325
gary <gary.white@lodgit.com>
2022-02-24 13:19:11 +0100

    code working

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
  function remainder(n, m){
   // Divide the larger argument by the smaller argument and return the remainder
-  return n % m;
+  if (m > n){
+      return m % n;
+  } else {
+      return n % m;
+  }
 }
 
 console.log(remainder(17,5)); // 2
+console.log(remainder(13,72)); // 7
+console.log(remainder(72,13)); // 7
+console.log(remainder(1,0)); // NAN
+console.log(remainder(0,0)); // NAN
